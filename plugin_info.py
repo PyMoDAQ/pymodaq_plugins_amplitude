7e2f4e9253aa3a0b58effdf090efe8ae08b84f65
Sébastien Weber <seba.weber@gmail.com>
2020-11-10 14:00:52 +0100

    Initial commit

---
+++ plugin_info.py
@@ -0,0 +1,13 @@
+## To modify by developper(s) of the plugin
+SHORT_PLUGIN_NAME = 'amplitude'  #for instance daqmx
+package_url = 'https://github.com/CEMES-CNRS/pymodaq_plugins_amplitude' #to modify
+description = 'Set of PyMoDAQ plugins for Amplitude Systems Lasers'
+
+author = 'Sébastien Weber'
+author_email = 'sebastien.weber@cemes.fr'
+
+#packages required for your plugin:
+packages_required = ['serial',
+                     'crcmod',
+                     ]
+##
